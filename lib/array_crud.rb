bd26528f600f2bcc3e7a5d069aee6c829948c9c5
Rick Glascock <rixong@gmail.com>
2020-01-09 02:46:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,48 @@
 def create_an_empty_array
-  []
+  arr = Array.new
 end
 
 def create_an_array
-  [2,4,6,8]
+  arr = Array.new
+  count = 0
+  letter = 'x'
+  until count == 4
+    arr.push(letter)
+    letter = arr[count] + 'x'
+    count += 1
+  end
+  p arr
+  
 end
 
-def add_element_to_end_of_array(array, element)
-  array << element
+def add_element_to_end_of_array(arr, ele)
+  arr << ele
+end
+  
+def add_element_to_start_of_array(arr, ele)
+  arr.unshift(ele)
 end
 
-def add_element_to_start_of_array(array, element)
-  array.unshift(element)
+def remove_element_from_end_of_array(arr)
+  arr.pop
 end
 
-def remove_element_from_end_of_array(array)
-  array.pop
+def remove_element_from_start_of_array(arr)
+  arr.shift
 end
 
-def remove_element_from_start_of_array(array)
-  array.shift
+def retrieve_element_from_index(arr, index)
+  arr[index]
 end
 
-def retrieve_element_from_index(array, index_number)
-  array[index_number]
+def retrieve_first_element_from_array(arr)
+  arr.first
 end
 
-def retrieve_first_element_from_array(array)
-  array[0]
+def retrieve_last_element_from_array(arr)
+  arr.last
 end
 
-def retrieve_last_element_from_array(array)
-  array[-1]
-end
-
-def update_element_from_index(array, index_number, element)
-array[index_number]=element
+def update_element_from_index(a, index, ele)
+    a[index] = ele
 end
